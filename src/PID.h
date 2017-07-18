7b2cc521bff9f9f80b1186ce812366d2fc546718
DIPRE <DIPRECXY@users.noreply.github.com>
2017-07-18 10:33:18 +0800

    final

--- src/PID.h
+++ src/PID.h
@@ -1,5 +1,17 @@
 #ifndef PID_H
 #define PID_H
+#define PID_STARTUP 300
+
+#define T_UP 1
+#define T_DOWN 2
+#define T_INIT 0
+#define MIN_RESOLUTION 0.1
+
+#include <uWS/uWS.h>
+#include <vector>
+#include <array>
+
+using namespace std;
 
 class PID {
 public:
@@ -10,37 +22,22 @@ public:
   double i_error;
   double d_error;
 
+  /* Local state */
+  int n;
+  double err;
+
+  /* Twiddle */
+  vector<double> p;
+  vector<double> dp;
+  double best_err;
+  int t_index, t_state;
+
   /*
   * Coefficients
   */ 
-  double _Kp;
-  double _Ki;
-  double _Kd;
-
-  /*
-  * Parameters for coefficients update
-  */
-  double _dKp;
-  double _dKi;
-  double _dKd;
-
-  double _update_threshold;
-  int _update_position = 0;
-  int    _n_step = 0;
-  bool   _is_first_loop = true;
-  double _best_error;
-  double _current_error;
-  double _total_error;
-
-  /*
-  * Position Parameters
-  */
-  double _x_trajectory = 0;
-  double _y_trajectory = 0;
-  double _orientation = 0;
-  double _steering_noise = 0;
-  double _distance_noise = 0;
-  double _steering_drift = 0;
+  double Kp;
+  double Ki;
+  double Kd;
 
   /*
   * Constructor
@@ -63,19 +60,49 @@ public:
   void UpdateError(double cte);
 
   /*
+   * Guess our control value.
+   */
+  double Guess();
+
+  /*
   * Calculate the total PID error.
   */
   double TotalError();
 
   /*
-  * Update coefficients by twiddle algorithm
-  */
-  void UpdateCoefficients();
+   * Restart our PID controller
+   */
+  void TwiddleInit();
 
   /*
-  * Update throttle based on steering angle
-  */
-  double UpdateThrottle(double steer_value);
+   * We finished a run, so update our error terms
+   * and reset our counters for the next Twiddle
+   * iteration.
+   *
+   */
+  void TwiddleUpdate();
+
+  /*
+   * We finished a run and have obtained a total error err.
+   * Take the next step in the twiddle algorithm, either
+   * 1) going UP on the value of parameter t_index,
+   * 2) going DOWN on the value of parameter t_index,
+   * 3) staying UP/DOWN and incrementing our delta by 1.1x
+   * 4) refining our delta to be finer grained by 0.9x
+   *
+   * If a given parameter t_index is changed by less than
+   * MIN_RESOLUTION, stay on that parameter until we've found
+   * something that works within MIN_RESOLUTION.
+   *
+   * We found this helpful as we first do our best to stabilize
+   * the gross updates with proportional error.  Once these
+   * are refined we step and repeat the same for the differential
+   * error.  Once that is fine, we skip to the integral error and
+   * repeat.  After all three parameters are at the same resolution,
+   * we repeat each in turn in search of optimum values.
+   *
+   */
+  void TwiddleStep(double err);
 
 };
 
